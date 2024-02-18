ffcc0d5c78f8c910a3f4f0d27feb7e2b6050aa42
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-02-18 04:54:44 -0500

    mong

--- backend/src/groups/device.ts
+++ backend/src/groups/device.ts
@@ -15,20 +15,20 @@ async function initializeRecommendedDatabase(userId: number, terraId: string) {
     }
   );
 
-  const nutrition_response = await fetch(
-    "https://api.tryterra.co/v2/nutrition?to_webhook=false&user_id=${terraId}",
-    {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        "dev-id": process.env.TERRA_DEV_KEY!,
-        "x-api-key": process.env.TERRA_API_KEY!,
-      },
-    }
-  );
+  //   const nutrition_response = await fetch(
+  //     "https://api.tryterra.co/v2/nutrition?to_webhook=false&user_id=${terraId}",
+  //     {
+  //       method: "GET",
+  //       headers: {
+  //         accept: "application/json",
+  //         "dev-id": process.env.TERRA_DEV_KEY!,
+  //         "x-api-key": process.env.TERRA_API_KEY!,
+  //       },
+  //     }
+  //   );
 
   const body = await body_response.json();
-  const nutrition = await nutrition_response.json();
+  //   const nutrition = await nutrition_response.json();
 
   const cardio_query: string = `I would like to analyze this data and give me a list of medications that you would recommend for me in the category of cardiovascular health.
     The format should be in json and should look like this:
@@ -40,26 +40,26 @@ async function initializeRecommendedDatabase(userId: number, terraId: string) {
     if you have no suggestions then return an empty array. Please do not add anything superfluous.
     Data: ${JSON.stringify(body)}`;
 
-  const meta_query: string = `I would like to analyze 2 data and give me a list of medications that you would recommend for me in the category of metabolic health.
-    The format should be in json and should look like this:
-    [{
-        medication_name: "",
-        reason_for_recommendation: "",
-        recommended_dosage: ""
-    }, ...]
-    if you have no suggestions then return an empty array. Please do not add anything superfluous.
-    Data1: ${JSON.stringify(body)}
-    Data2: ${JSON.stringify(nutrition)}`;
+  //   const meta_query: string = `I would like to analyze 2 data and give me a list of medications that you would recommend for me in the category of metabolic health.
+  //     The format should be in json and should look like this:
+  //     [{
+  //         medication_name: "",
+  //         reason_for_recommendation: "",
+  //         recommended_dosage: ""
+  //     }, ...]
+  //     if you have no suggestions then return an empty array. Please do not add anything superfluous.
+  //     Data1: ${JSON.stringify(body)}
+  //     Data2: ${JSON.stringify(nutrition)}`;
 
-  const rest_query: string = `I would like to analyze 2 data and give me a list of medications that you would recommend for me in the category of metabolic health.
-    The format should be in json and should look like this:
-    [{
-        medication_name: "",
-        reason_for_recommendation: "",
-        recommended_dosage: ""
-    }, ...]
-    if you have no suggestions then return an empty array. Please do not add anything superfluous.
-    Data1: ${JSON.stringify(body)}`;
+  //   const rest_query: string = `I would like to analyze 2 data and give me a list of medications that you would recommend for me in the category of metabolic health.
+  //     The format should be in json and should look like this:
+  //     [{
+  //         medication_name: "",
+  //         reason_for_recommendation: "",
+  //         recommended_dosage: ""
+  //     }, ...]
+  //     if you have no suggestions then return an empty array. Please do not add anything superfluous.
+  //     Data1: ${JSON.stringify(body)}`;
 
   const cardio_model_response = await fetch(
     "https://84d839fa-9eff-4a85-b8c7-cd5c88a917f3.monsterapi.ai/generate",
@@ -70,62 +70,42 @@ async function initializeRecommendedDatabase(userId: number, terraId: string) {
     }
   );
 
-  const meta_model_response = await fetch(
-    "https://84d839fa-9eff-4a85-b8c7-cd5c88a917f3.monsterapi.ai/generate",
-    {
-      headers: { Authorization: `Bearer ${process.env.MONSTER_KEY}` },
-      method: "POST",
-      body: JSON.stringify({ prompt: meta_query }),
-    }
-  );
+  //   const meta_model_response = await fetch(
+  //     "https://84d839fa-9eff-4a85-b8c7-cd5c88a917f3.monsterapi.ai/generate",
+  //     {
+  //       headers: { Authorization: `Bearer ${process.env.MONSTER_KEY}` },
+  //       method: "POST",
+  //       body: JSON.stringify({ prompt: meta_query }),
+  //     }
+  //   );
 
-  const rest_model_response = await fetch(
-    "https://api-inference.huggingface.co/models/medicalai/ClinicalBERT",
-    {
-      headers: { Authorization: `Bearer ${process.env.MONSTER_KEY}` },
-      method: "POST",
-      body: JSON.stringify({ prompt: rest_query }),
-    }
-  );
+  //   const rest_model_response = await fetch(
+  //     "https://api-inference.huggingface.co/models/medicalai/ClinicalBERT",
+  //     {
+  //       headers: { Authorization: `Bearer ${process.env.MONSTER_KEY}` },
+  //       method: "POST",
+  //       body: JSON.stringify({ prompt: rest_query }),
+  //     }
+  //   );
 
   const cardio_model = await cardio_model_response.json();
-  const meta_model = await meta_model_response.json();
-  const rest_model = await rest_model_response.json();
+  //   const meta_model = await meta_model_response.json();
+  //   const rest_model = await rest_model_response.json();
 
   const cardio = JSON.parse(cardio_model);
-  const meta = JSON.parse(meta_model);
-  const rest = JSON.parse(rest_model);
+  //   const meta = JSON.parse(meta_model);
+  //   const rest = JSON.parse(rest_model);
 
   db.medicationRecommendation.createMany({
-    data: [
-      {
-        userId,
-        recommendedDosage: cardio.recommended_dosage,
-        dateOfRecommendation: new Date(),
-        price: "0",
-        reasonForRecommendation: cardio.reason_for_recommendation,
-        medicationImage: "",
-        medicationName: cardio.medication_name,
-      },
-      {
-        userId,
-        recommendedDosage: meta.recommended_dosage,
-        dateOfRecommendation: new Date(),
-        price: "0",
-        reasonForRecommendation: meta.reason_for_recommendation,
-        medicationImage: "",
-        medicationName: meta.medication_name,
-      },
-      {
-        userId,
-        recommendedDosage: rest.recommended_dosage,
-        dateOfRecommendation: new Date(),
-        price: "0",
-        reasonForRecommendation: rest.reason_for_recommendation,
-        medicationImage: "",
-        medicationName: rest.medication_name,
-      },
-    ],
+    data: cardio.map((c: any) => ({
+      userId,
+      recommendedDosage: c.recommended_dosage,
+      dateOfRecommendation: new Date(),
+      price: "0",
+      reasonForRecommendation: c.reason_for_recommendation,
+      medicationImage: "",
+      medicationName: c.medication_name,
+    })),
   });
 }
 
